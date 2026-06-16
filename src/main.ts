9ea02ffa4b07d923e3ec4b53de8f9c1064189263
Marllon Wendel <m.w.p.ferreira@outlook.com>
2026-06-16 01:58:53 -0300

    fix: :bug: cofig local cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ZodValidationPipe } from 'nestjs-zod';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,OPTIONS,PATCH,POST,DELETE,PUT',
+    credencials: true,
+  });
+
   app.useGlobalPipes(new ZodValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
